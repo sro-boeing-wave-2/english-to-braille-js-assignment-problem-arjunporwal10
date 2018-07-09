23889d4bfdceca07c8512ed26b1e42b1b99f61ea
arjun <arjunporwal10@gmail.com>
2018-07-09 18:28:51 +0530

    linting and test cases passed

--- index.js
+++ index.js
@@ -5,3 +5,24 @@
  * in english language to that in the equivalent braille
  * code.
  */
+
+import englishToBrailleLiteralSet from './english-to-braille.js';
+
+function engtobrl() {
+  const str = document.getElementById('sourceLangText').value;
+  const splitstr = str.split('');
+  const sizeoftable = englishToBrailleLiteralSet.length;
+  const sizeofinputstring = splitstr.length;
+  let output = '';
+  for (let i = 0; i < sizeofinputstring; i += 1) {
+    for (let j = 0; j < sizeoftable; j += 1) {
+      if (splitstr[i] === englishToBrailleLiteralSet[j][0]) {
+        output += englishToBrailleLiteralSet[j][1];
+      }
+    }
+  }
+  document.getElementById('targetLangText').innerHTML = output;
+}
+window.onload = function () {
+  document.getElementById('btnConvertEnglishToBraille').addEventListener('click', engtobrl);
+};
